4fd4554ba3e7161a466451984a01929918ba3a22
Raimundo da Silva Sudário <raimundodasilvasudario@gmail.com>
2025-05-08 17:08:32 -0300

    Versão_1.3.1
    Projeto em desenvolvimento.

--- tarefas/urls.py
+++ tarefas/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.lista_tarefas, name="lista_tarefas"),
+    path("tarefa/<int:id>", views.tarefaView, name="tarefa-view")
+    
 
 ]
--- tarefas/views.py
+++ tarefas/views.py
@@ -1,5 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404 
+from .models import Tarefas
 
 # Create your views here.
 def lista_tarefas(request):
-    return render(request, "tarefas/list.html")
+    tarefas_list = Tarefas.objects.all().order_by('-created_at')
+
+    return render(request, "tarefas/list.html", {"tarefas":tarefas_list})
+
+def tarefaView(request, id):
+    taefa = get_object_or_404(Tarefas, pk=id)
+    return render(request, 'tarefas/tarefa.html', {'tarefa':taefa })
